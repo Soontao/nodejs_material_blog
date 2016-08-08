c46b2b4045f83c0a8f317069dba59336fe5e9f8c
suntao <mrls@live.cn>
2016-08-08 14:53:09 +0800

    提交dockerfile

--- blog/routes/admin.js
+++ blog/routes/admin.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.all('/', function(req, res, next) {
+router.all('/', function (req, res, next) {
     res.redirect("/admin/overview");
 });
 
-router.get("/overview", function(req, res, next) {
-    req.db.driver.execQuery("select view.viewname from usertypeview left join usertype on usertype.id = usertypeview.usertypeid left join view on usertypeview.viewid = view.id where usertype.id = ?", [req.session.user.usertype], function(err, data) {
+router.get("/overview", function (req, res, next) {
+    req.db.driver.execQuery("select view.viewname from usertypeview left join usertype on usertype.id = usertypeview.usertypeid left join view on usertypeview.viewid = view.id where usertype.id = ?", [req.session.user.usertype], function (err, data) {
         if (err) throw err;
         res.renderp("overview", {
             process: process,
@@ -15,15 +15,15 @@ router.get("/overview", function(req, res, next) {
     });
 });
 
-router.get("/costume", function(req, res, next) {
+router.get("/costume", function (req, res, next) {
     res.renderp("costume");
 });
 
-router.post("/costume", function(req, res, next) {
+router.post("/costume", function (req, res, next) {
     req.models.setting.find({
         key: "site"
-    }, function(err, values) {
-        values[0].save({ value: JSON.stringify(req.body) }, function(err) {
+    }, function (err, values) {
+        values[0].save({ value: JSON.stringify(req.body) }, function (err) {
             if (err) throw err;
             res.send("success");
         });
